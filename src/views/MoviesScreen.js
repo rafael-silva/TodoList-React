e41ceece6ff4a49d51b917cb01eb64e18cb32dd7
Rafael Silva <rafael_torrica@hotmail.com>
2021-12-01 18:27:56 -0300

    add request to download movies

--- src/views/MoviesScreen.js
+++ src/views/MoviesScreen.js
@@ -20,41 +20,29 @@ const MoviesScreen = () => {
             <Text> My favority movies </Text>
             { isLoading ? <ActivityIndicator/> :(
                 <FlatList
-                data={data}
-                keyExtractor={({ id }, index) => id}
-                renderItem={({ item }) => (
-                    <Text>{item.title}, {item.releaseYear}</Text>
-                  )}
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <Text>{item.title}, {item.releaseYear}</Text>
+                      )}
                 />
             )}
         </View>
     );
 };
 
-const getMyFavoriteMovies = async ({setData, setLoading}) => {
-    try {
-        const response = await fetch('https://reactnative.dev/movies.json');
-        const json = await response.json;
-        setData(json.movies)
-    } catch(erro) {
-        console.error('🛑 Error', erro)
-    } finally {
-        setLoading(false)
-    }
+const getMyFavoriteMovies = ({setData, setLoading}) => {
+    fetch('https://reactnative.dev/movies.json')
+    .then(response => response.json())
+    .then((data) => {
+        setData(data.movies)
+    })
+    .catch((erro) => {
+        console.error('🛑 Error', erro);
+    })
+    .finally(() => {
+      setLoading(false)
+    })
 };
-
-// const getMyFavoriteMovies = async ({setData, setLoading}) => {
-//     fetch('https://reactnative.dev/movies.json')
-//     .then(response => response.json())
-//     .then(MoviesJson => {
-//         const newLocal = setData(MoviesJson);
-//     })
-//     .catch(error => {
-//         console.log('Error', error);
-//     })
-//     .finally( () => {
-//          setLoading(false); 
-//     });
-// };
 
 export default MoviesScreen
